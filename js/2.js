ce8093c46836945bdcab77e93e6b01005267f526
Fawziasfq <732598573@qq.com>
2017-04-11 20:40:46 +0800

    2017/4/11

--- js/2.js
+++ js/2.js
@@ -10,19 +10,66 @@
 
 // })
 var vm = new Vue({
-	el: '#app',
+		el: '#app',
+		data: {
+			message: 3,
+			rawHtml: "<em>hello world!</em>",
+			dynamicId: true,
+			someDynamicCondition: false,
+			seen: false,
+			url: "www.baidu.com"
+		},
+		methods: {
+			doSomething: function() {
+				alert("hello world!")
+			}
+		}
+
+	})
+	//计算属性
+var app2 = new Vue({
+	el: '#app-2',
 	data: {
-		message: 3,
-		rawHtml: "<em>hello world!</em>",
-		dynamicId: true,
-		someDynamicCondition: false,
-		seen: false,
-		url: "www.baidu.com"
+		message: "hello"
+	}
+})
+
+var app3 = new Vue({
+	el: '#app-3',
+	data: {
+		message: "hello",
+		isActive: true,
+		hasError: false
 	},
-	methods: {
-		doSomething: function() {
-			alert("hello world!")
+	computed: {
+		reversedMessage: function() {
+
+
+			return this.message.split('').reverse().join('')
 		}
 	}
+})
+var app4 = new Vue({
+	el: '#app-4',
+	// //返回绑定对象的属性
+	// data: {
+	// 	classObject: {
+	// 		active: true,
+	// 		'text-danger': false
+	// 	}
+	// }
 
+	//返回绑定对象的计算属性
+	data: {
+		isActive: true,
+		error: null
+	},
+	computed: {
+		classObject: function() {
+			return {
+				active: this.isActive && !this.error,
+				'text-danger': this.error && this.error.type === 'fatal',
+			}
+		}
+	}
 })
