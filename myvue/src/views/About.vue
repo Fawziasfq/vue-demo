17887f7cd2b9f647e8878ad43c8ab5bc774cd04f
Fawziasfq <732598573@qq.com>
2019-06-19 16:38:34 +0800

    my vue project init by webpack

---
+++ myvue/src/views/About.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="home">
+        <h2>About</h2>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'About',
+
+    data() {
+        return {};
+    }
+}
+</script>
+<style lang="scss" scoped>
+
+</style>
